51ff50f37a7ff0a518f8f6bb50d45ff8dd6fb876
BomberUHD <dilvir@live.de>
2023-06-23 21:31:30 +0200

    init try for migration to dotnet

---
+++ suggestionsbot/EventHandler.cs
@@ -0,0 +1,77 @@
+﻿using DisCatSharp.Enums;
+using DisCatSharp.EventArgs;
+using DisCatSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DisCatSharp.Entities;
+using System.ComponentModel;
+
+namespace suggestionsbot
+{
+    [EventHandler]
+    public class MyEventHandler
+    {
+        [Event]
+        private static async Task MessageCreated(DiscordClient s, MessageCreateEventArgs e) 
+        {
+            ulong submission = 720695699737673768;
+            ulong review = 1083692771631702016;
+            ulong cacheaudit = 1121855303466614984;
+
+            DiscordChannel reviewchannel = s.GetChannelAsync(review).Result;
+            DiscordChannel cachechannel = s.GetChannelAsync(cacheaudit).Result;
+
+            DiscordAttachment attachment = e.Message.Attachments.FirstOrDefault();
+            
+            var x = DownloadFileAndSaveInCache(attachment.Url);
+            DiscordMessageBuilder cachemessage = new DiscordMessageBuilder();
+            cachemessage.WithFile($"{e.Author}_{e.Message.Id}", x.Result);
+            var fromCache = cachemessage.SendAsync(cachechannel);
+
+            if (e.Channel.Id == submission && !e.Author.IsBot)
+            {
+               DiscordEmbedBuilder embed = new DiscordEmbedBuilder();
+                embed.Title = e.Message.Author.UsernameWithGlobalName;
+                embed.Description = e.Message.Content;
+                embed.ImageUrl = fromCache.Result.Content.AttachedImageUrl();
+                
+                await s.SendMessageAsync(reviewchannel, embed);
+            }
+
+        }
+
+        static async Task<MemoryStream> DownloadFileAndSaveInCache(string fileUrl)
+        {
+            byte[] fileContent;
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync(fileUrl))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        using (Stream stream = await response.Content.ReadAsStreamAsync())
+                        using (MemoryStream memoryStream = new MemoryStream())
+                        {
+                            await stream.CopyToAsync(memoryStream);
+                            //fileContent = memoryStream.ToArray();
+                            //return fileContent;
+                            return memoryStream;
+                        }
+                    }
+                    else
+                    {
+                        // Handle the case where the file download was not successful
+                        Console.WriteLine("File download failed. Status code: " + response.StatusCode);
+                        return null;
+                    }
+                }
+            }
+        }
+
+
+    }
+
+}
---
+++ suggestionsbot/Program.cs
@@ -0,0 +1,35 @@
+﻿using DisCatSharp;
+using DisCatSharp.Entities;
+using DisCatSharp.Enums;
+using Microsoft.Extensions.DependencyInjection;
+using suggestionsbot;
+using System.Reflection;
+
+namespace MyFirstBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = "NDg1MTYzNTg4OTMyMjA2NjE0.GjmQgG.6z__6G4y--2KT42_2t13y26GdoMPnlASQ80O8k",
+                TokenType = TokenType.Bot,
+                ServiceProvider = new ServiceCollection().AddScoped<Program>().AddSingleton<MyEventHandler>().BuildServiceProvider(),
+                Intents = DiscordIntents.All
+                //Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContent
+            });
+            
+            //MyEventHandler handler = new();
+            //discord.RegisterEventHandler(handler);
+            discord.RegisterEventHandlers(Assembly.GetExecutingAssembly());
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
